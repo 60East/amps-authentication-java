471b3dc08215f002b33433357a125058be3b1ec0
Ryan Sheidow <ryan@crankuptheamps.com>
2018-10-18 18:03:08 -0400

    AC-827 first commit of a functioning java kerberos authenticator with a minimal test

--- AMPSKerberos/src/main/java/com/crankuptheamps/authentication/kerberos/AMPSKerberosAuthenticator.java
+++ AMPSKerberos/src/main/java/com/crankuptheamps/authentication/kerberos/AMPSKerberosAuthenticator.java
@@ -1,14 +1,82 @@
 package com.crankuptheamps.authentication.kerberos;
 
+import java.io.IOException;
+import java.security.PrivilegedActionException;
+import java.util.Base64;
+
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+
+import org.ietf.jgss.GSSContext;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.ietf.jgss.GSSManager;
+import org.ietf.jgss.GSSName;
+import org.ietf.jgss.Oid;
+
 import com.crankuptheamps.client.Authenticator;
 import com.crankuptheamps.client.exception.AuthenticationException;
+import com.sun.security.auth.callback.TextCallbackHandler;
 
 public class AMPSKerberosAuthenticator implements Authenticator {
+	private String _spn;
+	private GSSContext _secContext;
+
+	public AMPSKerberosAuthenticator(String spn_, String loginContextName_) throws AuthenticationException {
+		_spn = spn_;
+
+		try {
+			LoginContext ctx = new LoginContext(loginContextName_, new TextCallbackHandler());
+			ctx.login();
+
+			Subject.doAs(ctx.getSubject(), new java.security.PrivilegedExceptionAction<Object>() {
+				public Object run() throws IOException, AuthenticationException {
+					try {
+						acquireCredentials();
+					} catch (AuthenticationException e) {
+						throw new AuthenticationException(e);
+					}
+					return null;
+				}
+			});
+		} catch (LoginException | PrivilegedActionException e) {
+			throw new AuthenticationException(e);
+		}
+	}
+
+	private void acquireCredentials() throws AuthenticationException {
+		GSSManager manager = GSSManager.getInstance();
+		GSSName clientName;
+		try {
+			//TODO: Where should the user name come from? It should be attainable from the login context
+			clientName = manager.createName("60east", GSSName.NT_USER_NAME);
+			GSSCredential clientCreds = manager.createCredential(clientName, 8 * 3600, (Oid[]) null,
+					GSSCredential.INITIATE_ONLY);
+
+			GSSName peerName = manager.createName(_spn, GSSName.NT_HOSTBASED_SERVICE);
+			_secContext = manager.createContext(peerName, null, clientCreds, GSSContext.DEFAULT_LIFETIME);
+			_secContext.requestMutualAuth(true);
+		} catch (GSSException e) {
+			throw new AuthenticationException(e);
+		}
+	}
+
+	private byte[] initializeSecurityContext(byte[] inToken_) throws AuthenticationException {
+		if (inToken_ == null) {
+			inToken_ = new byte[0];
+		}
+		try {
+			return _secContext.initSecContext(inToken_, 0, inToken_.length);
+		} catch (GSSException e) {
+			throw new AuthenticationException(e);
+		}
+	}
 
 	@Override
 	public String authenticate(String username_, String currentPassword_) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
+		byte[] outToken = initializeSecurityContext(null);
+		return new String(Base64.getEncoder().encode(outToken));
 	}
 
 	@Override
--- AMPSKerberos/src/test/java/com/crankuptheamps/authentication/kerberos/AMPSKerberosAuthenticatorTest.java
+++ AMPSKerberos/src/test/java/com/crankuptheamps/authentication/kerberos/AMPSKerberosAuthenticatorTest.java
@@ -1,5 +1,9 @@
 package com.crankuptheamps.authentication.kerberos;
 
+import java.util.Properties;
+
+import com.crankuptheamps.client.exception.AuthenticationException;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +11,36 @@ import junit.framework.TestSuite;
 /**
  * Unit test for AMPSKerberosAuthenticator.
  */
-public class AMPSKerberosAuthenticatorTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AMPSKerberosAuthenticatorTest( String testName )
-    {
-        super( testName );
-    }
+public class AMPSKerberosAuthenticatorTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AMPSKerberosAuthenticatorTest(String testName) {
+		super(testName);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AMPSKerberosAuthenticatorTest.class );
-    }
+		Properties props = System.getProperties();
+		props.setProperty("sun.security.krb5.debug","true");
+		props.setProperty("java.security.krb5.conf", "/home/ryan/projects/ryan/kerberos/krb5-ec2-windows.conf");
+		props.setProperty("java.security.auth.login.config", "src/test/resources/jaas.conf");
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AMPSKerberosAuthenticatorTest.class);
+	}
+
+	/**
+	 * 
+	 * @throws AuthenticationException
+	 */
+	public void testObtainToken() throws AuthenticationException {
+		String spn = "AMPS@ip-172-31-5-55.us-west-2.compute.internal";
+		String loginContextName = "TestClientWindowsKDC";
+		AMPSKerberosAuthenticator authenticator = new AMPSKerberosAuthenticator(spn, loginContextName);
+		System.out.println(authenticator.authenticate(null, null));
+	}
 }
